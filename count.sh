9a860985f3c43ed238735f7a7ecc8d9765bd0fbf
wangshengwei@cmcm.com <wangshengwei@cmcm.com>
2018-04-04 20:10:20 +0800

    update

--- count.sh
+++ count.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-dt=`date -d '+8 hours' +"%Y%m%d"`
+dt=`date -d 'today' +"%Y%m%d"`
 echo $dt
 
 curl 127.0.0.1:5000/fanfou/public_timeline/count/${dt}
+
+count=`cat ${dt}_count.txt`
+content=${dt}_"${count}"
+
+curl 127.0.0.1:5000/fanfou/chifan/${content}
