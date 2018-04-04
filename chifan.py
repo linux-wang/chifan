bd78cfb701805091f0b42c926847f9de15a65fee
wangshengwei@cmcm.com <wangshengwei@cmcm.com>
2018-04-04 15:28:25 +0800

    add get_public_timeline

--- chifan.py
+++ chifan.py
@@ -8,14 +8,30 @@ except:
     from config import key, secret, username, password
 
 
-def tweet(content):
-    import fanfou 
+
+def get_client():
+    import fanfou
     consumer = {'key': key, 'secret': secret}
     client = fanfou.XAuth(consumer, username, password)
     fanfou.bound(client)
+    return client
+
+
+def tweet(content):
+    client = get_client()
     body = {'status': content}
     resp = client.statuses.update(body)
     return resp
+
+
+def get_public_timeline(count=10):
+    import json
+    client = get_client()
+    resp = client.statuses.public_timeline()
+    data = json.loads(resp.read())
+    
+    for fan in data:
+        print fan['text']
 
 
 @app.route('/')
