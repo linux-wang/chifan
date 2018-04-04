1eb1c197546bcc3eceaf2c0da944d7a2f00a4db1
wangshengwei@cmcm.com <wangshengwei@cmcm.com>
2018-04-04 19:21:11 +0800

    add count

--- chifan.py
+++ chifan.py
@@ -24,28 +24,55 @@ def tweet(content):
     return resp
 
 
-def get_public_timeline(count=10):
-    import json
-    client = get_client()
-    resp = client.statuses.public_timeline()
-    data = json.loads(resp.read())
-    
-    for fan in data:
-        print fan['text']
-
-
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
 
-@app.route('/fanfou/<content>', methods=['GET'])
+@app.route('/fanfou/chifan/<content>', methods=['GET'])
 def fanfou(content):
     resp = tweet(content)
     if resp.code == 200:
         return "SUCESS!"
     else:
         return "FAILED!"
+
+
+@app.route('/fanfou/public_timeline/get', methods=['GET'])
+def get_public_timeline():
+    import json
+    client = get_client()
+    resp = client.statuses.public_timeline()
+    data = json.loads(resp.read())
+
+    import time
+    output_file = 'data/' + time.strftime('%Y%m%d') + '.txt'
+    
+    for fan in data:
+        with open(output_file, 'a') as f:
+            f.write(str(fan['user']['id']).encode('utf8') + '\n')
+
+    return "WRITE SUCESS!"
+
+
+@app.route('/fanfou/public_timeline/count/<dt>', methods=['GET'])
+def get_user_num(dt):
+    file_name = 'data/' + dt + '.txt'
+    user_set = set()
+    message_num = 0
+
+    with open(file_name, 'r') as f:
+        for i in f.readlines():
+            if i.endswith('\n'):
+                i = i[:-1]
+            user_set.add(i)
+            message_num = message_num + 1
+
+    count_file = 'data/' + dt + '_count.txt'
+    with open(count_file, 'a') as f:
+        f.write(str(len(user_set)) + ' ' + str(message_num))
+
+    return "COUNT OVER!"
 
 
 if __name__ == '__main__':
